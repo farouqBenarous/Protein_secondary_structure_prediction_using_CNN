ab3a93e4e7f676ca38ab31715a4c7c07bea11349
farouk benarous <farouk.benarous26@gmail.com>
2020-10-04 00:14:46 +0100

    * format

--- Code/main.py
+++ Code/main.py
@@ -13,12 +13,13 @@ if __name__ == '__main__':
     # ---------------- Data loading , processing ---------------#
 
     print("loading the data ...")
-    dataset = load_dataset("../dataset/cullpdb+profile_6133.npy.gz",{'sequence_len': sequence_len, 'amino_acid_residues': amino_acid_residues,
-                                 'num_classes': num_classes,'total_features':total_features})
+    dataset = load_dataset("../dataset/cullpdb+profile_6133.npy.gz",
+                           {'sequence_len': sequence_len, 'amino_acid_residues': amino_acid_residues,
+                            'num_classes': num_classes, 'total_features': total_features})
 
     print("preprocessing the data ...")
-    X, Y = read_and_process(dataset,{'sequence_len': sequence_len, 'amino_acid_residues': amino_acid_residues,
-                                 'num_classes': num_classes})
+    X, Y = read_and_process(dataset, {'sequence_len': sequence_len, 'amino_acid_residues': amino_acid_residues,
+                                      'num_classes': num_classes})
     X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.20, random_state=2)
 
     # ---------------- Training / testing / infer the models ---------------#
